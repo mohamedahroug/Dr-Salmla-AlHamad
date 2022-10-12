dbedb4a770bf06dd30c359670df8c6a496b7533e
Mohamed Dahroug <83013635+mohamedahroug@users.noreply.github.com>
2022-10-12 21:23:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+const list = document.querySelector(".menu");
+const bars = document.querySelector(".bars");
+const scrolls = document.querySelectorAll(".scroll");
+
+bars.addEventListener("click",function(){
+    list.classList.toggle("show-menu");
+    bars.classList.toggle("close");
+})
+scrolls.forEach(function(scroll){
+    console.log(scroll)
+    scroll.addEventListener("cilck", function(e){
+        e.preventDefault();
+        console.log(e.currentTarget)
+    })
+})
+
+
+// search
+const search = document.querySelector(".search");
+const inSearch = document.querySelector(".in-search");
+
+search.addEventListener("click",function(){
+    inSearch.classList.toggle("show-search")
+    search.classList.toggle("x-search")
+})
+
+
+// slider
+
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlide = slides.length;
+let slideNumber = 0;
+// image slider next button
+nextBtn.addEventListener("click",function(){
+    slides.forEach(function(slide){
+        slide.classList.remove("active")
+    });
+    slideIcons.forEach(function(slideIcon){
+        slideIcon.classList.remove("active")
+    });
+
+
+    slideNumber++;
+
+
+    if(slideNumber > (numberOfSlide - 1)){
+        slideNumber = 0;
+    }
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+
+})
+
+// image slider prev button
+prevBtn.addEventListener("click",function(){
+    slides.forEach(function(slide){
+        slide.classList.remove("active")
+    });
+    slideIcons.forEach(function(slideIcon){
+        slideIcon.classList.remove("active")
+    });
+
+
+    slideNumber--;
+
+
+    if(slideNumber < 0){
+        slideNumber = numberOfSlide-1;
+    }
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+
+})
